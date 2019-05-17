ff5a685b3dd1b25e4b2f583d402e5e1501e814d0
Adam <adamdev718@gmail.com>
2019-05-17 11:48:56 +0800

    ios build guide 수정

---
+++ _upload.sh
@@ -0,0 +1,8 @@
+# export EXPO_DEBUG=true
+# export EXPO_APPLE_ID="designbak@naver.com"
+# export EXPO_APPLE_ID_PASSWORD="Stoy8429!@"
+export FASTLANE_ITC_TEAM_ID="117912098"
+export FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD="hnpn-kwyd-yjik-pycu"
+
+#expo build:ios
+expo upload:ios --apple-id designbak@naver.com --apple-id-password Stoy8429!@ --itc-team-id 117912098
