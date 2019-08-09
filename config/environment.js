afd086dfcd922917479a2d633b24bd3d5c338969
adam718 <adamdev718@gmail.com>
2019-08-09 17:42:47 +0800

    ocr key 변경.

--- config/environment.js
+++ config/environment.js
@@ -1,11 +1,11 @@
 export const Environment = {
-  GOOGLE_CLOUD_VISION_API_KEY: 'AIzaSyDhVreXc0wvIs3n_yAI1A3BcQHxdnthibI',
-  FIREBASE_API_KEY: 'AIzaSyDXyIUGS5t9VbP3HGxcIokYx9zfkLIb9Qs',
-  FIREBASE_AUTH_DOMAIN: 'sonic-terrain-245007.firebaseapp.com',
-  FIREBASE_DATABASE_URL: 'https://sonic-terrain-245007.firebaseio.com"',
-  FIREBASE_PROJECT_ID: 'sonic-terrain-245007',
-  FIREBASE_STORAGE_BUCKET: 'sonic-terrain-245007.appspot.com',
-  FIREBASE_MESSAGING_SENDER_ID: '819598946305'
+  GOOGLE_CLOUD_VISION_API_KEY: 'AIzaSyCTLkuQrM9w6kss3ZL8aVyHFkRPkldPGUg',
+  FIREBASE_API_KEY: 'AIzaSyB29FbJzXBXcCkk3ePiTIzTJUptFvsEZpg',
+  FIREBASE_AUTH_DOMAIN: 'blotch-ocr-storage.firebaseapp.com',
+  FIREBASE_DATABASE_URL: 'https://blotch-ocr-storage.firebaseio.com',
+  FIREBASE_PROJECT_ID: 'blotch-ocr-storage',
+  FIREBASE_STORAGE_BUCKET: 'blotch-ocr-storage.appspot.com',
+  FIREBASE_MESSAGING_SENDER_ID: '392343569664'
 };
 
 export default Environment;
