b2cade3eab408e11c4661fbdb32a0054835a0b91
David <comet0704@outlook.com>
2019-11-04 22:16:17 +0900

    react-native-gesture-handler 추가

--- _upload.sh
+++ _upload.sh
@@ -5,7 +5,7 @@ export FASTLANE_ITC_TEAM_ID="117912098"
 export FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD="hnpn-kwyd-yjik-pycu"
 
 # [주의] 이전의 인증서를 모두 삭제하고 새로 빌드됨, 특별히 삭제할 필요가 없다면 expo build:ios 만 이용.
-#expo build:ios --clear-credentials --revoke-credentials
+expo build:ios --clear-credentials --revoke-credentials
 
-expo build:ios
+#expo build:ios
 #expo upload:ios
